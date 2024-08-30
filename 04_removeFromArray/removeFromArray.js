cd53f6b4192663088a91bc4cb6c8dc273c56dea8
lieri123 <142183090+lieri123@users.noreply.github.com>
2024-08-30 19:51:33 -0400

    Create function for remveFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(my_arr, ...target) {
+    let length = my_arr.length
+
+    let new_arr = []
+
+    my_arr.forEach((items) => {
+
+        if(!target.includes(items)){
+            new_arr.push(items)
+        }
+        
+    });
+    return new_arr
 };
 
 // Do not edit below this line
